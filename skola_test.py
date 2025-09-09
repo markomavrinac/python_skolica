acfd0ee6bab3e2b092bd336615c91fd3a7223325
Marko Mavrinac <mavrinac07@gmail.com>
2025-09-09 10:13:44 +0200

    Add files via upload

---
+++ skola_test.py
@@ -0,0 +1,15 @@
+a = 2
+b = 3.4
+
+
+#ovo je neki komentar - jedna linija
+
+
+"""
+ovo je
+neki komentar
+u vise linija
+"""
+
+#ovje printamo nesto
+print("Vrijednost varijable a na kvadrat je",a**0.5)
